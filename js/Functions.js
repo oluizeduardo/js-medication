73f2097aa6c7e1afa1b0125f6d89bd7c0148599c
Luiz Eduardo <costaeduardoluiz@gmail.com>
2016-06-15 23:27:36 -0300

    Updating JavaScript

--- js/Functions.js
+++ js/Functions.js
@@ -5,14 +5,19 @@ var Functions = (function () {
     // It converts from String to Object.
     tblMedication = JSON.parse(tblMedication); 
     // If the list is empty, it initializes this list.
-    if (tblMedication === null) 
-      tblMedication = [];
+    if (tblMedication === null){
+        tblMedication = [];
+    } 
+
+    function gettblMedication(){
+        return tblMedication;
+    }
 
 
 
     function addMedication(event) {
         event.preventDefault();
-       /* var medication = JSON.stringify({
+        var medication = JSON.stringify({
         ID: $("#txtID").val(),
         Name: $("#txtName").val(),
         MedName: $("#txtMedName").val(),
@@ -24,20 +29,18 @@ var Functions = (function () {
         tblMedication.push(medication);
         localStorage.setItem("tblMedication", JSON.stringify(tblMedication));
         alert("Novo medicamento registrado!"); 
-        return true;*/
-        alert('DEU CERTO LUIZ!!');
+        return true;
+    }
+
+
+
+    function addListener(){
+        var btn = document.getElementById("btnAdd");
+        btn.addEventListener("click", addMedication);
     }
 
     
 
-    function addListener(){
-        //var btn = document.getElementById("btnAdd");
-       // btn.addEventListener("click", addMedication);
-        console.log('Chegou aqui!!');
-    }
-
-
     addListener();
-
-
+    
 })();
